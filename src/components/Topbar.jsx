6842724aaba72469c18a2efb796d36640e4c7b89
Gabriel Genê <gabrielgene97@gmail.com>
2018-09-29 14:22:04 -0300

    Initial commit

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class Topbar extends React.Component {
+  state = {
+    open: false,
+  }
+
+  toggleDrawer = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+
+  render() {
+    const { classes, menu, back, children, title } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            {
+              menu &&
+              <IconButton
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="Menu"
+                onClick={this.toggleDrawer}
+              >
+                <MenuIcon />
+              </IconButton>
+            }
+            {
+              back &&
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+            }
+            <Typography variant="title" color="inherit" className={classes.grow}>
+              {title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer open={this.state.open} onClose={this.toggleDrawer}>
+          <h1>Menu</h1>
+        </Drawer>
+        {children}
+      </div>
+    );
+  }
+}
+
+
+
+export default withStyles(styles)(Topbar);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from './pages/Register';
+import Topbar from './components/Topbar';
 import registerServiceWorker from './registerServiceWorker';
+
+const RegisterPage = () => (
+  <Topbar title="Cadastro">
+    <Register />
+  </Topbar>
+);
+
+const CulturePage = () => (
+  <Topbar title="Cultura">
+    <Register />
+  </Topbar>
+);
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={RegisterPage} />
+      <Route exact path="/cultura" component={CulturePage} />
+      <Route exact path="/progresso-cultura" component={Register} />
+      <Route exact path="/agricultores" component={Register} />
+    </Switch>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
