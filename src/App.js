f57e9122da80afe79ec7fceb34e0a7eb35fcbb9a
Rafael Melo <rsmelo_@hotmail.com>
2018-09-29 20:45:06 -0300

    Some additions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{maxWidth: 300}}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
