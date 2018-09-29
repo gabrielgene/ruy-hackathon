ec9fbaea053dcb41745d22d98c1af7f72dc0f74f
Rafael Melo <rsmelo_@hotmail.com>
2018-09-29 15:29:10 -0300

    Done 2

--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -5,9 +5,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom'
 import LinearProgress from '@material-ui/core/LinearProgress';
