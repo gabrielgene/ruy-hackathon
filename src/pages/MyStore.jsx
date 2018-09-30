e67899cfe70157168c73d5054ecb8b4e5df170cf
Gabriel Genê <gabrielgene97@gmail.com>
2018-09-30 08:46:04 -0300

    Refactor

--- src/pages/MyStore.jsx
+++ src/pages/MyStore.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Modal from '@material-ui/core/Modal';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   paper: {
     position: 'absolute',
-    top:200,
-    left:50,
-    right:0,
+    top: 200,
+    left: 50,
+    right: 0,
     width: '56%',
     height: 200,
     borderRadius: 8,
@@ -17,8 +19,15 @@ const styles = theme => ({
     textAlign: 'center',
     border: 'none',
   },
-  title:{
-    marginTop: -10,
+  wrapper: {
+    marginTop: 257,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    marginBottom: theme.spacing.unit * 3,
   },
   loader: {
     width: '80%',
@@ -28,30 +37,36 @@ const styles = theme => ({
 
 class MyStore extends Component {
   state = {
-    renderThat: true,
+    loading: true,
   }
-  render() {
-    const { classes } = this.props;
-    const { renderThat } = this.state;
+
+  componentDidMount() {
     setTimeout(() => {
-      this.setState({renderThat:false})
-    }, 3800);
-    return (
-      <div>
-        <h1>Minha loja</h1>
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={renderThat}
-        >
-          <div className={classes.paper}>
-            <h2 className={classes.title}>Carregando Informações</h2>
-            <img className={classes.loader} src="https://i.imgur.com/dgDIOKt.gif" alt="loading" />
+      this.setState({ loading: false });
+    }, 3000);
+  }
+
+  render() {
+    const { loading } = this.state;
+    const { classes } = this.props;
+    if (loading) {
+      return (
+        <div>
+          <div className={classes.wrapper} >
+            <Typography variant="title" gutterBottom className={classes.text}>
+              Estamos preparando a sua loja para você...
+            </Typography>
+            <CircularProgress className={classes.progress} size={50} />
           </div>
-        </Modal>
-        {/* {renderThat && <p>Migué!</p>} */}
-      </div>
-    )
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Minha loja</h1>
+        </div>
+      );
+    };
   }
 }
 
